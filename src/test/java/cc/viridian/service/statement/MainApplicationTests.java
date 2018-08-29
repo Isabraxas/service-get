a1942ba87150bc3b06aad5a3ad8257bb42e30270
Fernando Ontiveros <feronti@gmail.com>
2018-08-29 10:43:51 -0400

    removing jackson library from pom.xml

--- src/test/java/cc/viridian/service/statement/MainApplicationTests.java
+++ src/test/java/cc/viridian/service/statement/MainApplicationTests.java
@@ -1,5 +1,6 @@
 package cc.viridian.service.statement;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,5 +12,6 @@ public class MainApplicationTests {
 
     @Test
     public void contextLoads() {
+        Assert.assertEquals(1,1);
     }
 }
