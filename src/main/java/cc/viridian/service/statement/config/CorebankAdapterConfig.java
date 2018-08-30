22a21b642aa37fc3b6b42b331b1192481c7b8a66
Fernando Ontiveros <feronti@gmail.com>
2018-08-30 15:14:49 -0400

    error handling when get properties from config server

--- src/main/java/cc/viridian/service/statement/config/CorebankAdapterConfig.java
+++ src/main/java/cc/viridian/service/statement/config/CorebankAdapterConfig.java
@@ -1,44 +1,54 @@
 package cc.viridian.service.statement.config;
 
+import cc.viridian.provider.AdapterConfig;
 import cc.viridian.provider.CoreBankProvider;
-import cc.viridian.provider.CorebankConfig;
 import cc.viridian.provider.spi.CoreBank;
+import java.net.URL;
 import java.util.HashMap;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@Configuration
 public class CorebankAdapterConfig {
 
-    private HashMap<String, CorebankConfig> loadedClasses;
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
-    public HashMap<String, CorebankConfig> getLoadedClasses() {
+    @Value("${spring.cloud.config.uri}")
+    private String springCloudConfigUrl;
+
+    private HashMap<String, AdapterConfig> loadedClasses;
+
+    public HashMap<String, AdapterConfig> getLoadedClasses() {
         return loadedClasses;
     }
 
     /**
-     * Initialize corebanks.
-     * @return
+     * Initialize corebank adapters.
+     *
+     * @return list of valid adapters
      */
     @Bean
-    public HashMap<String, CorebankConfig> init() {
+    public HashMap<String, AdapterConfig> initializeAdapters() {
         try {
-            CoreBankProvider coreBank = CoreBankProvider.getInstance();
-            loadedClasses = coreBank.init();
+            CoreBankProvider coreBankProvider = CoreBankProvider.getInstance();
+            //loadedClasses = coreBankProvider.initializeAdapters();
+            loadedClasses = coreBankProvider.getAdapters();
             if (loadedClasses.size() == 0) {
                 log.error("Fatal Error. There are zero Corebank adapters loaded in the system.");
                 log.error("Check load class path to include valid Corebank adapters.");
                 System.exit(1);
             }
 
-            log.info("Corebank Adapters:");
-            for (CorebankConfig config : loadedClasses.values()) {
-                log.info(config.getName() + " " + config.getClassName());
-                String profile = "default";
-                String configServerUrl = "http://desarrollo.viridian.cc:7000";
-                config.loadConfigProperties(profile, configServerUrl);
+            for (AdapterConfig config : loadedClasses.values()) {
+                config.loadConfigProperties(activeProfile, springCloudConfigUrl);
             }
         } catch (Exception e) {
             e.printStackTrace();
